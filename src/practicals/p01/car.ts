4bf0b8cf9a5e8a3daa2e9c6a97d98a29db2c6463
DarkyMCPJ <harumi.ploy@gmail.com>
2026-02-20 17:37:42 +0700

    1st attempt

--- src/practicals/p01/car.ts
+++ src/practicals/p01/car.ts
@@ -1,1 +1,22 @@
-export class Car {}
+export class Car {
+
+brand: string;
+model: string;
+
+start(): void{
+console.log(`${this.brand} ${this.model} is starting...`);
+}
+
+acceraate(): void{
+console.log(`${this.brand} ${this.model} is accelerating...`);
+}
+
+brake(): void{
+console.log(`${this.brand} ${this.model} is braking...`);
+}
+
+stop(): void{
+console.log(`${this.brand} ${this.model} is stopping...`);
+}
+
+}
